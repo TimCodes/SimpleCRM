15248331899ce8c3d775f5638b831bcc360204d5
Tim Hardy <thardy@sbt.local>
2018-08-03 18:21:18 -0600

    forgot to save changes to user

--- SimpleCrm/Models/User.cs
+++ SimpleCrm/Models/User.cs
@@ -7,5 +7,13 @@ namespace SimpleCrm.Models
 {
     public class User
     {
+        public int id { get; set; }
+        public string Title { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public string Email { get; set; }
+        public string HashedPass { get; set; }
+        public List<string> roles { get; set; }
     }
 }
